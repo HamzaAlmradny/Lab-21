56124e189db8e0433e4003d14ce07606a9c06b55
Dr. Alsayed <Dr. Alsayed@DESKTOP-APIOASQ>
2025-07-29 21:22:13 +0300

    Add project files.

---
+++ Lab 21/Lab 21.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	
+	int x=0;
+
+	for (x = 0;x < 10;x++) {
+		cout << x << endl;
+	}
+	
+	
+	
+	
+	//while (x < 10) {
+	// cout << x << endl;
+	//x++;
+	//}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+
+}
+
